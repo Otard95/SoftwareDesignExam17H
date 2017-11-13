9ca663143327d04487280e01648efeb059a1aac9
Stian Myklebostad <stian.myklebostad@getmail.no>
2017-11-13 19:41:29 +0100

    Buy() DeliverItem() update StartStore() started

--- Bot_B/Store.cs
+++ Bot_B/Store.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Bot_B {
 	class Store {
 
+		private Object _lock;
+
 		private List<Iitem> _items;
 		private Log _log;
+		private bool _running;
 
-		public List<Iitem> Items { get {
+		public List<Iitem> Items {
+			get {
 				return _items;
 			}
 		}
@@ -22,21 +26,51 @@ namespace Bot_B {
 			_items = new List<Iitem>();
 			_log = Log.Instance;
 
-		}
-
-		public Iitem Buy (int _item_id) {
-
-
+			_running = true;
 
 		}
 
-		public void DeliverItem (Iitem _item) {
+		public Iitem Buy (Iitem item) {
 
+			lock (_lock) {
 
+				if (_items.Contains(item)) {
+					int index = _items.IndexOf(item);
+					Iitem item_to_return = _items[index];
+					_items.Remove(item);
+					return item_to_return;
+				} else {
+					return null;
+				}
+
+			}
 
 		}
 
-		public void StartStore () { }
+		public void DeliverItem (Iitem item) {
+
+			lock (_lock) {
+
+				_items.Add(item);
+
+			}
+
+		}
+
+		public void StartStore () {
+
+			while (_running || _items.Count > 0) {
+
+				if (_running) {
+					Iitem new_item = ItemFactory.CreateRandom("Temp Name", 999.99, "Temp Desc"); // TODO: Name generation
+					DeliverItem(new_item);
+				}
+				
+				Thread.Sleep(500);
+
+			}
+
+		}
 
 		// public List<Iitem> CommitBurglary (int _skill) { }
 
