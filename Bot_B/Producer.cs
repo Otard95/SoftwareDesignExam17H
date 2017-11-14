edfebea62c01b0eab87297829a7c5512fbf91d85
Stian Myklebostad <stian.myklebostad@getmail.no>
2017-11-14 08:30:31 +0100

    fixes

--- Bot_B/Producer.cs
+++ Bot_B/Producer.cs
@@ -11,8 +11,7 @@ namespace Bot_B {
 		private List<List<ItemProperties>> _presets;
 		private List<Store> _stores;
 
-		public Producer (List<Store> stores, string name)
-		{
+		public Producer (List<Store> stores, string name) {
 			_name = name;
 			_stores = stores;
 			_presets = null;
@@ -27,12 +26,13 @@ namespace Bot_B {
 		}
 
 		private void SendItem () {
-		var rnd = new Random();
-			var itemNumber = rnd.Next(_stores.Count);
+			var rng = new Random();
+			int recieving_store = rng.Next(_stores.Count);
+			int preset = rng.Next(_presets.Count); 
 
-			Item itemToSend = ItemFactory.CreateSpecific()
+			Item item_to_send = ItemFactory.CreateSpecific(_presets[preset));
 
-_stores[itemNumber].DeliverItem();
+			_stores[recieving_store].DeliverItem(item_to_send);
 		}
 
 	}
