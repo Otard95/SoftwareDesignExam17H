dff16a0ea61910c688ae79fd9ddc6e863f04ad75
Stian Myklebostad <stian.myklebostad@getmail.no>
2017-11-21 16:24:51 +0100

    Pause when finished

--- Bot_B/Program.cs
+++ Bot_B/Program.cs
@@ -7,8 +7,13 @@ using System.Threading.Tasks;
 namespace Bot_B {
 	class Program {
 		static void Main (string[] args) {
+
 			var b1 = new Bot_B();
 			b1.Start();
+
+			Console.WriteLine("\nPress any key to continue ...");
+			Console.ReadKey();
+
 		}
 	}
 }
